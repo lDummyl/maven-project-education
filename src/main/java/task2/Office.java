28fa40aacd2fea038aaf5c1c7a5760501a630f10
Dima Ludov <lDummyl>
2019-07-27 21:38:03 +0300

    -add second task description -add third task description

---
+++ src/main/java/task2/Office.java
@@ -0,0 +1,20 @@
+package task2;
+
+public class Office {
+
+	/*
+	Secretary secretary;
+
+	Director director;
+
+	Hr hr;
+	*/
+
+	//претенденты идут по одному, когда их достаточно,
+	// то диретор принимает решение и берет одного в штат
+
+	void invitePeaople(Object human){
+
+	}
+
+}
