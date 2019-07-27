643c136d5368bb2f02985657e3075dd4cbc2091e
Dima Ludov <lDummyl>
2019-07-27 22:04:15 +0300

    -add 4,5,6 task description

---
+++ src/main/java/task6/Main.java
@@ -0,0 +1,14 @@
+package task6;
+
+/*
+	Реализовать алгоритм интерполяционного многочлена Лагранжа
+	по данной ссылке использовать реализацию C# и адаптировать ее в синтаксис Java
+	https://ru.wikibooks.org/wiki/%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8_%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC%D0%BE%D0%B2/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BF%D0%BE%D0%BB%D1%8F%D1%86%D0%B8%D1%8F/%D0%9C%D0%BD%D0%BE%D0%B3%D0%BE%D1%87%D0%BB%D0%B5%D0%BD_%D0%9B%D0%B0%D0%B3%D1%80%D0%B0%D0%BD%D0%B6%D0%B0
+
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
