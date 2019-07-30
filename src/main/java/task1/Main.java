9b1be04e6cf18c6da01eb6603d5bd977e27c4296
Serebrovskiy <13.spb@mail.ru>
2019-07-30 18:40:05 +0400

    Update Main.java

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -3,8 +3,10 @@ package task1;
 
 //задание
 
+import java.util.stream.IntStream;
+
 public class Main {
 	public static void main(String[] args) {
-
+		IntStream.range(0,9).forEach(i -> System.out.print("Hello World"));
 	}
 }
