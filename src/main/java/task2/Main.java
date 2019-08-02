f94f375cda86de1eef97b73c003ed1bddf9a2e32
Serebrovskiy <13.spb@mail.ru>
2019-08-02 15:33:13 +0400

    Director chooses secretary

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -1,4 +1,7 @@
 package task2;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 //Сделать 10 секретарей 1 hr и директора котторый
 // выбирает рандомно секретаря в штат, которого ему пердлагает HR,
@@ -6,6 +9,19 @@ package task2;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
+        ArrayList<Object> secretaries = new ArrayList<Object>();
+		Office office = new Office();
+		Director director = new Director();
+		Hr hr = new Hr();
+
+		hr.setJobSeekers(secretaries);  //hr встречается с секретарями
+
+		IntStream.range(0,10).forEach(i -> office.invitePeaople(hr.getJobSeeker().get(i)));  //передаем в офис по одному секретарю
+
+		director.directorsChoice(office.getInvitePeaople());  //дир принял всех секретарей
+
+		System.out.println("Решение директора: секретарь " + (director.randomIndex+1) + " - " + director.directorsDecision());   //решение директора
+
 	}
 }
--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -1,4 +1,9 @@
 package task2;
+import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class Office {
 
@@ -13,8 +18,53 @@ public class Office {
 	//претенденты идут по одному, когда их достаточно,
 	// то диретор принимает решение и берет одного в штат
 
-	void invitePeaople(Object human){
+//	Director director = new Director();
+// 	Hr hr = new Hr();
 
+	ArrayList<Object> incoming = new ArrayList<Object>();
+
+	int countSecr = 0;
+
+	void invitePeaople(Object human){      //претенденты приходят по одному
+
+		incoming.add(human);
+		System.out.println("Секретарь " + ++countSecr + " " + human);
 	}
 
+	ArrayList<Object> getInvitePeaople(){
+		return incoming;
+	}
+}
+
+class Secretary {
+}
+
+class Director  {
+	ArrayList<Object> incomingDir;
+	Random choice = new Random();
+	int randomIndex=0;
+
+	public void directorsChoice(ArrayList<Object> incomingDir){  //дир выбрирает секретаря
+
+		this.incomingDir = incomingDir;
+		randomIndex =  choice.nextInt(this.incomingDir.size());
+	}
+
+	public Object directorsDecision()  {        //озвучивает решение
+		return incomingDir.get(randomIndex);
+	}
+}
+
+class Hr {
+	ArrayList<Object> listJobSeekers;
+
+	void setJobSeekers(ArrayList<Object> listJobSeekers){    //Hr принял секретарей
+
+        this.listJobSeekers = listJobSeekers;
+		IntStream.range(0,10).forEach(i ->this.listJobSeekers.add(new Secretary()));  //познакомился
+	}
+
+	ArrayList<Object> getJobSeeker(){    //отправил список
+		return listJobSeekers;
+	}
 }
