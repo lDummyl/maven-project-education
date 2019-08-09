2918a92de8d76daf055256566e3de153b0885035
Dima Ludov <lDummyl>
2019-08-09 12:02:17 +0300

    - add bonus board solution

--- src/main/java/bonus/TestOfBoard.java
+++ src/main/java/bonus/TestOfBoard.java
@@ -6,6 +6,9 @@ package bonus;
 // и доставать их пользуясь данными методами
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TestOfBoard {
 	public static void main(String[] args) {
 		Board board = new Board(1000, 1000);
@@ -15,20 +18,38 @@ class TestOfBoard {
 	}
 }
 
+
 class Board {
 
-//	private final int xSize;
-//	private final int ySize;
+	private final int xSize;
+	private final int ySize;
+	private ArrayList<ArrayList<Object>> board = new ArrayList<>();
 
 	public Board(int xSize, int ySize) {
+		this.xSize = xSize;
+		this.ySize = ySize;
+		for (int i = 0; i < xSize; i++) {
+			board.add(getListOfNulls());
+		}
+	}
 
+	private ArrayList<Object> getListOfNulls() {
+		ArrayList<Object> ys = new ArrayList<>();
+		for (int j = 0; j < ySize; j++) {
+			ys.add(null);
+		}
+		return ys;
 	}
 
 	public Object get(int x, int y) {
-		return null;
+		return board.get(x).get(y);
 	}
 
 	public void put(int x, int y, Object toAdd) {
-
+		List<Object> xs = board.get(x);
+		if (xs == null) {
+			xs = getListOfNulls();
+		}
+		xs.add(y, toAdd);
 	}
 }
