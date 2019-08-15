2d328579ce2885d3808a73591961920be3772569
dmitrii.ludov <dmitrii.ludov@betoola.com>
2019-08-15 10:04:09 +0300

    add task 1 solution

--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -1,20 +1,79 @@
 package task2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class Office {
 
-	/*
 	Secretary secretary;
+	Director director = new Director(this);
+	Hr hr = new Hr("Аделаида Марковна");
 
-	Director director;
+	void invitePeople(Object human) {      //претенденты приходят по одному
+		if (human instanceof WantAWork) {
+			List<WantAWork> currentCandidates = hr.considerCandidate((WantAWork) human);
+			secretary = director.chooseSecretary(currentCandidates);
+		}
+	}
+}
 
-	Hr hr;
-	*/
+class Secretary implements WantAWork {
 
-	//претенденты идут по одному, когда их достаточно,
-	// то диретор принимает решение и берет одного в штат
+	String name;
 
-	void invitePeaople(Object human){
-
+	public Secretary(String name) {
+		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return "Секретарь " + name;
+	}
+
+	@Override
+	public boolean passInterview() {
+		return true;
+	}
+}
+
+
+class Director {
+
+	final Office myOffice;
+	final int enoughCandidatesToDecide = 4;
+	Random random = new Random();
+
+	public Director(Office myOffice) {
+		this.myOffice = myOffice;
+	}
+
+	public Secretary chooseSecretary(List<WantAWork> currentCandidates) {
+		if (currentCandidates.size() < enoughCandidatesToDecide) return null;
+		WantAWork wantAWork = currentCandidates.get(random.nextInt(currentCandidates.size()));
+		if (wantAWork instanceof Secretary) {
+			return (Secretary) wantAWork;
+		} else {
+			throw new RuntimeException("Что это вы, "+ myOffice.hr.name +",  мне подсовываете " + wantAWork.getClass() + " вместо " + Secretary.class);
+		}
+	}
+}
+
+class Hr{
+
+	List<WantAWork> listJobSeekers = new ArrayList<>();
+
+	String name;
+
+	public Hr(String name) {
+		this.name = name;
+	}
+
+	public List<WantAWork> considerCandidate(WantAWork candidate) {
+		if (candidate.passInterview()) {
+			listJobSeekers.add(candidate);
+		}
+		return listJobSeekers;
+	}
 }
