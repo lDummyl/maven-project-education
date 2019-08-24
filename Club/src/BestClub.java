7850346ac899d216b0bf996f197c43836fe3e8af
SuperFlanker93 <s-w-d@mail.ru>
2019-08-24 17:46:44 +0300

    Best CLub

---
+++ Club/src/BestClub.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BestClub {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите клуб:");
+        String club = in.nextLine();
+        System.out.println(club + " - лучший в мире клуб! ");
+    }
+}
