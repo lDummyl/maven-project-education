18a8681c59877a9cc0b1ee44841279dc5688766d
sinister524 <sinister524@gmail.com>
2019-10-23 19:39:42 +0300

    Перенес логику из теста на прошлом занятии (упражнение 2)

--- src/test/java/task2/OfficeTest.java
+++ src/test/java/task2/OfficeTest.java
@@ -2,13 +2,19 @@ package task2;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 
 public class OfficeTest {
 
 	// TODO: 10/22/19 не забудь перенести логику из теста ктотрый мы сделали.
 	@Test
-	public void invitePeople() {
+	public void inviteToAudition() {
+
 		Office office = new Office();
+		office.inviteToAudition(new Person("Larisa", "56"));
+//        assertTrue(office.getSecretary() != null);
+		assertNotNull(office.getSecretary());
 
 	}
 }
