896cf4581a92ab1e1466979765639bae34930d9a
dmitrii.ludov <dmitrii.ludov@betoola.com>
2019-10-31 11:25:07 +0300

    and one comment

--- src/main/java/task2/Secretary.java
+++ src/main/java/task2/Secretary.java
@@ -6,6 +6,9 @@ import task2.Skills.DocumentArchiving;
 
 public class Secretary implements AnswerThePhone, CanMakeCoffee, DocumentArchiving {
 
+    // TODO: 10/31/19 Есть хороший принцип yagni почитай про него. Это конечно тренеровочная площадка, но тренеровать и этот принцип тоже желательно
+    //  пока задача секретаря уметь проходить интервью.
+
     // TODO: 10/25/19 хочешь сказать что аггрегация лучше наследвания? Справедливо, но еще лучше интерфейсы.
 
     private String name;
