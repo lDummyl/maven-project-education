b9df35dd41fd41ee0e2452ed8d43c0a4fa2485f4
dmitrii.ludov <dmitrii.ludov@betoola.com>
2019-11-18 15:57:57 +0300

    and  comment

--- src/test/java/developer/task/FileMonitoringServiceTest.java
+++ src/test/java/developer/task/FileMonitoringServiceTest.java
@@ -11,14 +11,17 @@ public class FileMonitoringServiceTest {
 
     // TODO: 11/18/19 тесты тесты тесты, нужен кейс где один и тот же юзер обрабатывется несколькими потоками и извстный результат подтверждается, то есть не искривляется асинхронностью.
     
-    @SneakyThrows
+//    @SneakyThrows
     @Test
     public void runTest () {
         // TODO: тест проходит, но по факту он в потоке падает и ошибку не выдает. а через main в классе "FileMonitoringService" все работает нормально. С чем это связанно?
+
+        // TODO: 11/18/19 ну так это и работает, эксепшн в одном потоке на другой не влияет, для этого есть Callable. Чтобы тест прошел или нет нужен ассерт на твой результат и все будет пучком.
         String path = "./developer-task-logs";
         int countThread = 10;
 
         Thread thread = new Thread(new FileMonitoringService(path, countThread));
         thread.start();
+
     }
 }
