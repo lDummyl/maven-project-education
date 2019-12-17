b8c7cac1ed6bfd8c4893c13687e1d7dcc7f41d90
dmitrii.ludov <dmitrii.ludov@betoola.com>
2019-12-17 21:13:08 +0300

    refactor main method

--- src/main/java/bonus/ReadAndWrite.java
+++ src/main/java/bonus/ReadAndWrite.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.OptionalDouble;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,12 +21,9 @@ public class ReadAndWrite {
     private static final String petya = "Petya";
 
     // TODO: 12/13/19 в данном случае не важно какая коллекция, важно что добавление идет атомарной синхронизированной операцией
-        static Map<String, Report> averageSalary = new HashMap<>();
+    static Map<String, Report> averageSalary = new HashMap<>();
 //    static Map<String, Report> averageSalary = new ConcurrentHashMap<>();
 
-    static {
-        averageSalary.put(petya, new Report());
-    }
 
     public static void main(String[] args) {
 
@@ -42,7 +38,6 @@ public class ReadAndWrite {
         System.out.println("Duration sequential = " + Duration.between(start, LocalTime.now()).getSeconds());
         Double averageSequential = averageSalary.get(petya).average;
         System.out.println("Sequential average = " + averageSequential);
-        averageSalary.put(petya, new Report());
 
         start = LocalTime.now();
         petyaAvoirs.parallelStream().forEach(p -> {
@@ -59,7 +54,8 @@ public class ReadAndWrite {
     }
 
     @SneakyThrows
-    @Synchronized // TODO: 12/13/19 если синхронизировать эту операцию то профита по времени не будет. При этом если убрать то не изменится и результат.
+    @Synchronized
+    // TODO: 12/13/19 если синхронизировать эту операцию то профита по времени не будет. При этом если убрать то не изменится и результат.
     private static void doLotsOfWork() {
         Thread.sleep(100);
     }
@@ -69,7 +65,12 @@ public class ReadAndWrite {
     // TODO: 12/13/19 только здесь это важно, тебе в руки дают журнал только чтобы ты успел "найти последнюю строчку и вписать себя" и сразу отдать следующему,
     //  а не на день с ним походить в магазин в кино и потом сделать запись и отдать.
     private static void syncWrite(PayCheck p) {
-        Report report = averageSalary.get(p.name);
+        Report report = averageSalary.merge(p.name, new Report(p.summ, 1, (double) p.summ), (a, b) -> {
+            a.payTimes += b.payTimes;
+            a.summ += b.summ;
+            a.average = (double) a.summ / a.payTimes;
+            return a;
+        });
         report.payTimes++;
         report.summ += p.summ;
         report.average = report.summ / (double) report.payTimes;
@@ -90,5 +91,10 @@ public class ReadAndWrite {
         Integer payTimes = 0;
         Double average = 0.;
 
+        public Report(Integer summ, Integer payTimes, Double average) {
+            this.summ = summ;
+            this.payTimes = payTimes;
+            this.average = average;
+        }
     }
 }
--- src/main/java/developer/task/XMLInteraction/XMLParser.java
+++ src/main/java/developer/task/XMLInteraction/XMLParser.java
@@ -7,8 +7,13 @@ import developer.task.structureXML.output.supportClasses.UserSite;
 import lombok.Getter;
 
 import java.io.File;
-import java.time.*;
-import java.util.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 public class XMLParser {
@@ -32,7 +37,17 @@ public class XMLParser {
 
         UserSite userSite = new UserSite(log.getUserId(), log.getUrl());
         Map<LocalDate, UserIndicators> timeSpentPart = userSiteMap.get(userSite);
-        if ( timeSpentPart == null) {
+
+        // TODO: 12/17/19 тебе только остется проработать иницаилаизацию как следует чтобы без NPE и чтобы корректное добавление было
+        //  и кстати как у тебя сделано, если чувак просидел с 23-00 до 2-00 след дня это сколько вхождений 1-2?.
+        Map<LocalDate, Map<UserSite, UserIndicators>> impossibleMap = new HashMap<>();
+        for (Map.Entry<LocalDate, UserIndicators> entry : timeSpentPart.entrySet()) {
+            Map<UserSite, UserIndicators> siteIndiMap = new HashMap<>();
+            siteIndiMap.put(userSite, entry.getValue());
+            impossibleMap.put(entry.getKey(), siteIndiMap);
+        }
+
+        if (timeSpentPart == null) {
             userSiteMap.put(userSite, timeSpentFromLog);
         } else {
             for (Map.Entry<LocalDate, UserIndicators> entry : timeSpentFromLog.entrySet()) {
