68bfe749f1c1ee451d32e9d5302b35627029c333
Irina Shushpannikova <s89636497512@gmail.com>
2020-01-10 19:46:17 +0300

    111

--- src/main/java/task2/Director.java
+++ src/main/java/task2/Director.java
@@ -9,15 +9,23 @@ public class Director {
     public void giveBonus(Map<String, Double> kpis){
         double max = -1.0;
         String winner = "";
-        for (Map.Entry<String, Double> pair : kpis.entrySet()) {
-            Double value = pair.getValue();
-            if (value > max) {
-                max = value;
-                winner = pair.getKey();  //Что делать, если два максимальных КПИ?
-            }
 
+        if (kpis.size() >= 5) {
+            for (Map.Entry<String, Double> pair : kpis.entrySet()) {
+
+
+                Double value = pair.getValue();
+                if (value > max) {
+                    max = value;
+                    winner = pair.getKey();  //Что делать, если два максимальных КПИ?
+                }
+            }
+            System.out.println("Director: \"По результатам таблицы сотрудник " + winner + " получит премию\"");
         }
-        System.out.println("Director: \"По результатам таблицы сотрудник " + winner + " получит премию\"");
+        else
+        {
+            System.out.println("Director: \"Сотрудников должно быть >= 5\"");
+        }
     }
 }
 
