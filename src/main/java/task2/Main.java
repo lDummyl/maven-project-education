847a2201127af6ee39a8018ebdef62e323535f9b
John_Smith <the.tylertyler@gmail.com>
2020-01-19 22:07:34 +0300

    firs message

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -5,7 +5,7 @@ package task2;
 // все это происходит в офисе
 
 public class Main {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
