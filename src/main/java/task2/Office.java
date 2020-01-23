c832a75c50f9e5910367fd90b37ef68f78885c24
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-01-24 00:02:39 +0300

    add new bonus task fix

--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -22,6 +22,8 @@ public class Office {
     HR hr = new HR("Elena");
 
     public void createEmployees() {
+
+	    // TODO: 1/23/20 не уверен что ты понимаешь что делаешь
     	employees.put(director.name, director);
     	employees.put(hr.name, hr);
     	employees.put("Сandidate1",secretaries);
@@ -34,7 +36,17 @@ public class Office {
     	employees.put("Сandidate8",secretaries);
     	employees.put("Сandidate9",secretaries);
     	employees.put("Сandidate10",secretaries);
-
+	    // TODO: 1/23/20 и знаешь что самое интересное по сути secretaries это один и тот же объект на всех, гляди
+	    Collection<CompanyEmployees> values = new ArrayList<>(employees.values());
+	    values.remove(hr);
+	    values.remove(director);
+	    for (CompanyEmployees value : values) {
+	    	if(secretaries == value){
+			    System.out.println("Its the same object!");
+		    }
+	    }
+	    // TODO: 1/23/20 в данном случае map тебе не нужен. Заведи List с кандидатами в методе main и передавай его в офис.
+	    //  Где ты видел офис в котором рождаются люди? Они туда приходят извне.
     }
 
     public void random2(){
