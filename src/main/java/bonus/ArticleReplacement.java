8fbe3e71ba0e32c8450959536304fe3684c15424
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-01-26 21:02:13 +0300

    add new bonus task

---
+++ src/main/java/bonus/ArticleReplacement.java
@@ -0,0 +1,15 @@
+package bonus;
+
+// TODO: 1/26/20 Заменить все артикли "а" кроме первого и последнего, заменять следует только артикли
+//  буквы "а" в составе слов замене не подлежат.
+
+
+public class ArticleReplacement {
+
+	public static void main(String[] args) {
+
+		String s = "a creature is a  small part of a big world";
+		String a = "a";
+		String b = "the";
+	}
+}
