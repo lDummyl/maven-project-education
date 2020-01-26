95e5e1e1883db25e3cc448c2b0cd9cec8e785fbe
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-01-26 18:42:23 +0300

    change 1 task

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,11 +1,34 @@
 package task1;
 
 
-//задание вывести на печать Hello World 10 раз в одну строчку
-// hint: use stream Api
 
 public class Main {
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		// TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
+
+		// TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
 	}
+}
+
+class Name {
+
+	String firstName;
+	String lastName;
+
+}
+
+class Age {
+
+	int year;
+	int month;
+	int day;
+
+}
+
+class Person {
+
+	Name name;
+	Age age;
+
 }
