09237a4540bdae0aec1ec7f87d066f42da2939cd
Irina Shushpannikova <s89636497512@gmail.com>
2020-03-09 15:19:10 +0300

    задание 2 на четные числа и задание 3 на простые числа по лямбда-выражениям

--- src/main/java/bonus/LambdaHW2.java
+++ src/main/java/bonus/LambdaHW2.java
@@ -1,30 +1,70 @@
 package bonus;
-import java.io.IOException;
+
 import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
-import java.util.function.Predicate;
+
 
 public class LambdaHW2 {
     public static void main(String[] args) {
-        List<Integer> integers = Arrays.asList(1, 2, 3, 14, -4);
-        Calculation2 operation = (a, b) -> a+b;
-        int sumOfEvenNumbers = getSumOfEvenNumbers(integers, operation);
-        System.out.println("sumOfEvenNumbers = " + sumOfEvenNumbers);
-        int multiplication = getSumOfEvenNumbers(integers, (a, b) -> a * b);// TODO: 3/8/20 а если так, будет не сумма а произведение. Сделай метод более универсальным.
-        System.out.println("multiplication = " + multiplication);
+        List<Integer> integers = Arrays.asList(-2, 2, -1, 5, 12, -4, 11);
+        Calculation2 operation = (a, b) -> a + b;
+        int sumOfEvenNumbers = getOperationResultOfEvenNumbers(integers, operation); //находим сумму четных чисел
+        int sumOfPrimes = getOperationResultOfPrimes(integers, operation);     //находим сумму простых чисел
+        System.out.println("Сумма четных чисел коллекции: " + sumOfEvenNumbers);
+        System.out.println("Сумма простых чисел коллекции: " + sumOfPrimes);
+
+        int multiplication = getOperationResultOfEvenNumbers(integers, (a, b) -> a * b); //находим произведение четных
+        int multiplicationOfPrimes = getOperationResultOfPrimes(integers, (a, b) -> a * b);  //находим произведение простых
+        System.out.println("Произведение четных чисел коллекции:" + multiplication);
+        System.out.println("Произведение простых чисел коллекции: " + multiplicationOfPrimes);
     }
 
-    private static int getSumOfEvenNumbers(List<Integer> integers, Calculation2 operation) {
-        int result = 0; // TODO: 3/8/20 чтобы сильно тебя не мучать, вот подсказка. Если лист не пустой, то инициализировать можно первым элементом.
+    private static int getOperationResultOfPrimes(List<Integer> integers, Calculation2 operation) {  // для простых чисел
+        Integer result = null;
+
         for (Integer integer : integers) {
-            if (integer % 2 == 0){
-                result = operation.calculate2(result, integer);
+            if (isPrimeNumber(integer)) {
+                if (result == null) {
+                    result = integer;
+                } else {
+                    result = operation.calculate2(result, integer);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isPrimeNumber(int n) {  // метод для нахождения простых чисел
+        boolean prime = true;
+        for (long i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n <= 1)
+                return false;
+            if (n == 2)
+                return true;
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return (n % 2 != 0 && prime && n > 2) || n == 2;
+    }
+
+    private static int getOperationResultOfEvenNumbers(List<Integer> integers, Calculation2 operation) {  //для четных чисел
+        //integers.removeIf(element -> element % 2 != 0);   // удалим нечетные элементы из коллекции
+        Integer result = null;
+        for (Integer integer : integers) {
+            if (integer % 2 == 0) {
+                if (result == null) {
+                    result = integer;
+                } else {
+                    result = operation.calculate2(result, integer);
+                }
             }
         }
         return result;
     }
-
 }
 
 @FunctionalInterface
