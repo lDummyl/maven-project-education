72eccf90b3fbb41c8397dc4a4cdc3c4402081182
Irina Shushpannikova <s89636497512@gmail.com>
2020-03-17 19:35:46 +0300

    Конечный вариант структуры нового офиса

--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -16,11 +16,13 @@ public class Office {
 
     void work(Boss boss) {
         this.boss = boss;
-        System.out.println("Босс говорит бухгалтеру коэффициент сотрудника для дальнейшего подсчета премии: " + boss.tellAccountantFactorForBonus(hr.KPI));
+        System.out.println("");
+        System.out.println("Босс на основе KPI говорит бухгалтеру коэффициент сотрудника для дальнейшего подсчета премии: " + boss.tellAccountantFactorForBonus(hr.KPI));
     }
 
     void work(Employee employee) {
         this.employee = employee;
+        System.out.println("");
         System.out.println("Сотрудник сообщает ставку бухгалтеру: " + employee.rate);
         System.out.println("Сотрудник сообщает кол-во отработанных дней бухгалтеру: " + employee.workingHours);
         System.out.println("Сотрудник сообщает кол-во часов переработки бухгалтеру: " + employee.overtimeHours);
@@ -31,9 +33,10 @@ public class Office {
     void work(Accountant accountant, Employee employee) {
         this.accountant = accountant;
         this.employee = employee;
-        System.out.println("Подсчет бухгалтером зарплаты сотрудника: " + accountant.countSalary(employee.workingHours, employee.rate));
-        System.out.println("Подсчет бухгалтером надбавки за переработанные часы: " + accountant.countOvertimeBonus(employee.overtimeHours, employee.rate));
-        System.out.println("Подсчет бухгалтером бонуса за проект: " + accountant.countProjectBonus(employee.rate, boss.factorForBonus));
+        System.out.println("");
+        System.out.println("Бухгалтер считает зарплату сотрудника: " + accountant.countSalary(employee.workingHours, employee.rate));
+        System.out.println("Бухгалтер считает надбавки за переработанные часы: " + accountant.countOvertimeBonus(employee.overtimeHours, employee.rate));
+        System.out.println("Бухгалтер считает бонус за проект: " + accountant.countProjectBonus(employee.rate, boss.factorForBonus));
     }
 
 }
