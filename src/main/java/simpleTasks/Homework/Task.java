958f15e3ad23d58918c71336290aaa6ff59a4762
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-04-11 00:03:36 +0300

    add some comment

--- src/main/java/simpleTasks/Homework/Task.java
+++ src/main/java/simpleTasks/Homework/Task.java
@@ -8,14 +8,14 @@ public class Task {
     int taskNumber;
     String taskText;
     boolean isDone;
-    ArrayList<Task> tasksCollection = new ArrayList<>();
+    ArrayList<Task> tasksCollection = new ArrayList<>(); // TODO: 4/10/20 от так лучше не делать. В яблоке еще яблоки это странно.
 
     public Task(int taskNumber, String taskText) {
         this.taskNumber = taskNumber;
         this.taskText = taskText;
     }
 
-    public void attemptDoTask(){
+    public void attemptDoTask(){ // TODO: 4/11/20 и это тоже странно, задача сама себя не делает. Хочешь придумать ей поведение, она может распечататься или сравнится с другой посложности, раз, два.
         int times = 5;
         if(isDone){
             times = 0;
