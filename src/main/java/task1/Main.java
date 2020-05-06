7bc3a06a6284f0bcbba2216e5cdb14de98cc23a4
LilyLillet <49074481+LilyLillet@users.noreply.github.com>
2020-05-06 16:14:31 +0300

    attempt to solve task 1 part 1

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,7 +1,7 @@
 package task1;
 
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -19,14 +19,16 @@ public class Main {
         Age age = new Age(2010, 11, 5);
         Person person1 = new Person(name, age);
         Person person2 = new Person(new Name("Inna"), new Age(1998, 4, 14));
-        Person person3 = new Person(new Name("Inna1"), new Age(1993, 3, 14));
-        Person person4 = new Person(new Name("Inna2"), new Age(1993, 4, 14));
+        Person person3 = new Person(new Name("Grace"), new Age(1993, 2, 9));
+        Person person4 = new Person(new Name("Max"), new Age(1993, 2, 1));
+        Person person5 = new Person(new Name("Ingrid"), new Age(1993, 4, 14));
 
         ArrayList<Person> list = new ArrayList<>();
         list.add(person1);
         list.add(person2);
         list.add(person3);
         list.add(person4);
+        list.add(person5);
 
         Person oldest = list.get(0);
         for (Person person : list) {
@@ -39,11 +41,57 @@ public class Main {
 
         System.out.println("Старший это - " + oldest);
     }
+    public static int randomYear ()
+    {
+        int min = 1920;
+        int max = 2010;
+        int diff = max - min;
+        Random random = new Random();
+        int i = random.nextInt(diff);
+        return i + min;
+    }
+    public static int randomMonth ()
+    {
+        int min = 1;
+        int max = 12;
+        int diff = max - min;
+        Random random = new Random();
+        int i = random.nextInt(diff);
+        return i + min;
+    }
+    public static int randomDay ()
+    {
+        int min = 1;
+        int max = 30;
+        int diff = max - min;
+        Random random = new Random();
+        int i = random.nextInt(diff);
+        return i+ min;
+    }
 
     public static void second() {
         // TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
 
-        // TODO: 5/4/20 Привет! 
+        // TODO: 5/4/20 Привет!
+        ArrayList<Name> listOfNames = new ArrayList<>();
+        listOfNames.add(new Name("Ella", "Brown"));
+        listOfNames.add(new Name("Kate", "Stevenson"));
+        listOfNames.add(new Name("Mike", "Berry"));
+        listOfNames.add(new Name("Harry", "Kane"));
+        listOfNames.add(new Name("Kevin", "Smith"));
+        HashMap<Name, Age> persons = new HashMap<>(listOfNames.size());
+
+        int i = 0;
+        do {
+            Random random = new Random();
+            persons.put(listOfNames.get(random.nextInt(listOfNames.size())), new Age (randomYear(), randomMonth(), randomDay()));
+            i++;
+        }
+        while (i<listOfNames.size());
+
+        for (Map.Entry<Name, Age> nameAgeEntry : persons.entrySet()) {
+            System.out.println(nameAgeEntry);
+        }
 
     }
 
@@ -57,6 +105,12 @@ class Name {
     public Name(String name) {
         firstName = name;
     }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
 
     @Override
     public String toString() {
@@ -108,10 +162,6 @@ class Person {
 
     public boolean isOlderThan(Person oldest) {
 //		oldest 1900 this 1990
-        if (oldest.age.year < age.year) {
-            return false;
-        } else{
-            return true;
-        }
+        return oldest.age.year > age.year || oldest.age.month >= age.month || oldest.age.day >= age.day;
     }
 }
