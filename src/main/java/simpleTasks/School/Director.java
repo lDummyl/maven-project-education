ca89cbf54f4beeaa489bb0c5c039c777705eb04c
Irina Shushpannikova <s89636497512@gmail.com>
2020-05-11 22:00:39 +0300

    удалили студента версия2

--- src/main/java/simpleTasks/School/Director.java
+++ src/main/java/simpleTasks/School/Director.java
@@ -12,8 +12,10 @@ public class Director extends Person {
     public void doPaperWork() {
     }
     public void expelStudent(Student student) {
-
-
+        Map<Integer, List<Student>> allStudentsByGrade = school.allStudentsByGrade;
+        for (List<Student> studentList : allStudentsByGrade.values()) {
+            studentList.remove(student);
+        }
     }
 
     public void checkStudentsMarks() {
