b096cde92a5e79a0f3c1d30b55123145dab6aec8
Vasily <leonidasway11051989@gmail.com>
2020-05-13 21:01:35 +0300

    init

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,45 +1,165 @@
 package task1;
 
-
+import java.time.LocalDate;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		first();
-		second();
+    public static void main(String[] args) {
+        first();
+        second();
+    }
 
-	}
+    public static void first() {
+        // TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
+        List<Person> listPersons = new ArrayList<Person>();
+        Person person = new Person(new Name("Vasily", "S"), new Age(1989, 11, 11));
+        Person person2 = new Person(new Name("Ivan", "K"), new Age(2010, 11, 5));
+        Person person3 = new Person(new Name("Sergey", "M"), new Age(1980, 9, 2));
+        Person person4 = new Person(new Name("Nina", "I"), new Age(1984, 3, 5));
+        Person person5 = new Person(new Name("Masha", "O"), new Age(1990, 6, 7));
+        listPersons.add(person);
+        listPersons.add(person2);
+        listPersons.add(person3);
+        listPersons.add(person4);
+        listPersons.add(person5);
 
-	public static void first(){
-		// TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
+        LocalDate date = LocalDate.now();
+        int number = 0;
+        for (int i = 0; i < listPersons.size(); i++) {
+            LocalDate localDateComparable = LocalDate.of(listPersons.get(i).age.year, listPersons.get(i).age.month, listPersons.get(i).age.day);
+            if (date.isBefore(localDateComparable)) {
+                date = date;
+            } else {
+                date = localDateComparable;
+                number = i;
+            }
+        }
+        System.out.println(listPersons.get(number).name);
+        System.out.println();
+    }
 
-	}
+    public static void second() {
+        // TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
+        List listAges = new ArrayList<Age>();
+        listAges.add(new Age(2010, 11, 5));
+        listAges.add(new Age(1989, 5, 11));
+        listAges.add(new Age(1980, 9, 2));
+        listAges.add(new Age(1984, 3, 5));
+        listAges.add(new Age(1990, 6, 7));
+        listAges.add(new Age(1999, 7, 1));
+        listAges.add(new Age(2002, 8, 2));
+        listAges.add(new Age(2000, 1, 17));
 
-	public static void second(){
-		// TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
+        List listNames = new ArrayList<Name>();
+        listNames.add(new Name("Nina", "I"));
+        listNames.add(new Name("Masha", "O"));
+        listNames.add(new Name("Sergey", "M"));
+        listNames.add(new Name("Ivan", "K"));
+        listNames.add(new Name("Vasily", "S"));
+        listNames.add(new Name("Ruslan", "Z"));
+        listNames.add(new Name("Irina", "P"));
+        listNames.add(new Name("Tanya", "E"));
 
-	}
+        // LIST
+//        List list = new ArrayList<Person>();
+//        while (1==1){
+//            list.add(new Person(listNames.get(new Random().nextInt(7)), listAges.get(new Random().nextInt(7))));
+//            if (list.size()==5){
+//                break;
+//            }
+//        }
+//
+//        for (Object o:list
+//             ) {
+//            System.out.println(o);
+//        }
+
+        // SET
+//        Set set = new HashSet<Person>();
+//        while (1==1){
+//            set.add(new Person(listNames.get(new Random().nextInt(7)), listAges.get(new Random().nextInt(7))));
+//            if (set.size()==5){
+//                break;
+//            }
+//        }
+//        for (Object o:set
+//             ) {
+//            System.out.println(o);
+//        }
+
+        // MAP
+        Map map = new HashMap();
+        while (1 == 1) {
+            map.put(listNames.get(new Random().nextInt(7)), listAges.get(new Random().nextInt(7)));
+            if (map.size() == listNames.size()) {
+                break;
+            }
+        }
+
+        for (Object o : map.entrySet()
+        ) {
+            System.out.println(o);
+        }
+    }
 
 }
 
 class Name {
 
-	String firstName;
-	String lastName;
+    String firstName;
+    String lastName;
 
+    @Override
+    public String toString() {
+        return "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'';
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
 
 class Age {
 
-	int year;
-	int month;
-	int day;
+    int year;
+    int month;
+    int day;
+
+
+    public Age(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                '}';
+    }
 
 }
 
 class Person {
 
-	Name name;
-	Age age;
+    Name name;
+    Age age;
+
+    public Person(Object name, Object age) {
+        this.name = (Name) name;
+        this.age = (Age) age;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName='" + name.firstName + '\'' +
+                ", lastName='" + name.lastName + '\'' +
+                ", age=" + age.year + " " + age.month + " " + age.day + '\'';
+    }
 
 }
--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -1,5 +1,8 @@
 package task2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Office {
 
 	/*
@@ -10,11 +13,34 @@ public class Office {
 	Hr hr;
 	*/
 
-	//претенденты идут по одному, когда их достаточно,
-	// то диретор принимает решение и берет одного в штат
+    //претенденты идут по одному, когда их достаточно,
+    // то диретор принимает решение и берет одного в штат
 
-	void invitePeaople(Object human){
+    void invitePeople(Object human) {
+        List list = new ArrayList<Secretary>();
+        Secretary secretary = new Secretary("Nina", "I");
+        Secretary secretary2 = new Secretary("Masha", "O");
+        Secretary secretary3 = new Secretary("Sergey", "M");
+        Secretary secretary4 = new Secretary("Ivan", "K");
+        Secretary secretary5 = new Secretary("Vasily", "S");
+        Secretary secretary6 = new Secretary("Ruslan", "Z");
+        Secretary secretary7 = new Secretary("Irina", "P");
 
-	}
+        list.add(secretary);
+        list.add(secretary2);
+        list.add(secretary3);
+        list.add(secretary4);
+        list.add(secretary5);
+        list.add(secretary6);
+        list.add(secretary7);
 
+
+        new Director().finalChoose(new Hr().chooseSecretary(list));;
+    }
+
+    public static void main(String[] args) {
+    new Office().invitePeople(new Object());
+
+
+    }
 }
