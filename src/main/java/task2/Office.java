7a16a97d8ab1294af502cc15bb5f05c5ac659f5e
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-05-15 16:11:42 +0300

    add some comments

--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -17,7 +17,12 @@ public class Office {
     // то диретор принимает решение и берет одного в штат
 
     void invitePeople(Object human) {
-        List list = new ArrayList<Secretary>();
+        // TODO: 5/15/20 параметер должен использоваться
+
+        // TODO: 5/15/20 офис не создает секретарей, это работа для вселенной, бога или колледжа, офис только принимает и отбирает их
+        // TODO: 5/15/20 не забываем дженерики для интерфейса и даймонд оператор для конструктора
+          List list = new ArrayList<Secretary>();
+
         Secretary secretary = new Secretary("Nina", "I");
         Secretary secretary2 = new Secretary("Masha", "O");
         Secretary secretary3 = new Secretary("Sergey", "M");
@@ -35,10 +40,12 @@ public class Office {
         list.add(secretary7);
 
 
+        // TODO: 5/15/20 это же касается и директора
         new Director().finalChoose(new Hr().chooseSecretary(list));;
     }
 
     public static void main(String[] args) {
+        // TODO: 5/15/20 в классе Main уже есть метод, лучше в него перенести создание
     new Office().invitePeople(new Object());
 
 
--- src/main/java/task3/Oracul.java
+++ src/main/java/task3/Oracul.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Oracul {
+    // TODO: 5/15/20 оракул не должен быть статическим он может менять ключевые аспекты поведения в зависимости от того какими полями инициализирован его инстанс
     public static void oraculAnswer() {
         int x = questionLength();
         if (x > 1) {
@@ -13,15 +14,15 @@ public class Oracul {
         } else if (x == 1) {
             try {
                 if (new Random().nextInt(3) == 2) {
-                    int z = new Random().nextInt(6000);
-                    System.out.println("Дай подумать оракулу " +  z/100 + " секунд!");
+                    int z = new Random().nextInt(6000); // TODO: 5/15/20 каждый раз создавать новый рандом плохая практика
+                    System.out.println("Дай подумать оракулу " +  z/100 + " секунд!"); // TODO: 5/15/20 сколько оракул думает это не плохая характеристика(поле) его как объекта класса
                     Thread.sleep(z);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.println(getAnswer());
-        } else if (x == 0) {
+        } else if (x == 0) { // TODO: 5/15/20 сделай методы проверки checkForLength, ... и вынеси в них логику
             System.out.println("Не слышу вопроса в твоих речах");
         }
         oraculAnswer();
@@ -30,6 +31,7 @@ public class Oracul {
     public static int questionLength() {
         String s = new Scanner(System.in).nextLine();
         if (s.length() > 50) {
+            // TODO: 5/15/20 всегда нужно отделать ввод-логика-вывод. что если нужно будет версию в выводом в окно, везде sout менять придется?
             System.out.println("Будь лаконичней!");
             return -1;
         }
@@ -44,6 +46,7 @@ public class Oracul {
     public static int quantityOfQuestions(String s) {
         String[] q = s.trim().replaceAll("\\pP", "").split(" ");
         int count = 0;
+        // TODO: 5/15/20  бы сделал лучше мапу соответствия вопрос ответ или вопрос-список ответов
         for (int i = 0; i < q.length; i++) {
             if (q[i].equals("Что")) {
                 count++;
@@ -77,6 +80,8 @@ public class Oracul {
         answers.add("Не бойся, что не знаешь — бойся, что не учишься.");
         answers.add("Незнание — это не проблема, если есть желание учиться.");
         answers.add("Зачем ты задаешь такие странные вопросы, глупец?!!");
+        // TODO: 5/15/20 все же должно быть соответствие на вопрос как не стоит отвечат так же как на когда,
+        //  но вариантов ответа на каждый вопрос может быть несколько, например так Map<Question, List<Answer>>
         return answers.get(new Random().nextInt(answers.size()));
     }
 
