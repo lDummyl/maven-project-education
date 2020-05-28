587f07f1f7dbaa4c5404a29560be63fef5bd4aed
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-05-28 14:39:06 +0300

    add comments

--- src/main/java/task2/Hr2.java
+++ src/main/java/task2/Hr2.java
@@ -5,6 +5,8 @@ import task1.Person;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: 5/28/20 как ты можешь наблюдать в PersonProvider, для дого чтобы generify метод, совсем не обязательно generify весь класс.
+//  В данном случае уж точно не стоит создавать экземпляр HR под определенную профессию. Кроме того замени аггрегацию Person на наследование.
 public class Hr2<T> { //Экспериментальный класс
 
     public static final int YEAR_OF_BIRTH = 2000;
--- src/main/java/task2/OfficeWorker.java
+++ src/main/java/task2/OfficeWorker.java
@@ -5,6 +5,8 @@ import task1.Person;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: 5/28/20 тут совсем generic не нужен. строго говоря без них прекрасно можно обойтись и в PersonProvider, кода будет столько
+//  же, единственная функция дженериков это перевод runtime ошибок в compiletime ошибки. Все! По объему кода где-то 5% сокращает за счет потери downcast.
 public abstract class OfficeWorker<T>  {
 
     Person person;
