961db06e507c1ff252f5e7a16c71c9e2932b78f8
Vasily <leonidasway11051989@gmail.com>
2020-06-03 17:21:31 +0300

    init

--- src/main/java/task1/BirthDate.java
+++ src/main/java/task1/BirthDate.java
@@ -19,26 +19,6 @@ public class BirthDate {
 //        8 * 8 * 120 * 12 *28
     }
 
-    public BirthDate(int year, int month, int day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-    }
-
-    public static List<BirthDate> getBirthDates() {
-        List<BirthDate> birthDates;
-        birthDates = new ArrayList<>();
-        birthDates.add(new BirthDate(2001, 11, 5));
-        birthDates.add(new BirthDate(1980, 9, 2));
-        birthDates.add(new BirthDate(1984, 3, 5));
-        birthDates.add(new BirthDate(1990, 6, 7));
-        birthDates.add(new BirthDate(1989, 5, 11));
-        birthDates.add(new BirthDate(1999, 7, 1));
-        birthDates.add(new BirthDate(2002, 8, 2));
-        birthDates.add(new BirthDate(2000, 1, 17));
-        return birthDates;
-    }
-
     public LocalDate toLocalDate() {
         return LocalDate.of(year, month, day);
     }
