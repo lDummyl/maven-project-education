c1c11c176d942dc7b2f79c3b1350cef145ee66e2
rbl4a <rbl4a@icloud.com>
2020-07-15 22:47:47 +0500

    first commit

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,19 +1,43 @@
 package task1;
 
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO: 7/15/20 hi from outside
-
 		first();
 		second();
 
 	}
 
+
+
 	public static void first(){
 		// TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
+		ArrayList<Person> people = new ArrayList<>();
+
+
+		Person иванПетров = new Person("Иван Петров", 13, 8, 1956);
+		people.add(иванПетров);
+		people.add(new Person("Иван Иванов", 12, 5, 1985));
+		people.add(new Person("Иван Иванов", 12, 5, 1915));
+
+		Person oldestPerson = people.get(0);
+		for (Person person : people) {
+			boolean answer =  oldestPerson.isOlderThan(person);
+			if (!answer) {
+				oldestPerson = person;
+			}
+		}
+		Name name = new Name("a", "b");
+		System.out.println(oldestPerson.birthDate.year);
+
 
 	}
 
@@ -24,27 +48,48 @@ public class Main {
 
 }
 
+@ToString
+@AllArgsConstructor
 class Name {
 
 	String firstName;
 	String lastName;
 
 
-
-
 }
 
-class Age {
+class BirthDate {
 
 	int year;
 	int month;
 	int day;
+	LocalDate localDate;
 
+	public BirthDate(int day, int month, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		localDate = LocalDate.of(year, month ,day)
+	}
+
+	public boolean earlyThan(BirthDate birthDate) {
+		return this.localDate.isBefore(birthDate.localDate)
+		//return this.year< birthDate.year;
+	}
 }
 
 class Person {
 
 	Name name;
-	Age age;
+	BirthDate birthDate;
 
+
+	public Person(String name, int day, int month, int year) {
+		birthDate = new BirthDate(day, month, year);
+		// TODO: 15.07.2020 implement
+	}
+
+	public boolean isOlderThan(Person person) {
+		return this.birthDate.earlyThan(person.birthDate);
+	}
 }
