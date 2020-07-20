9db5bce0978acdb517e8caf5949c5d244300e0b5
rbl4a <rbl4a@icloud.com>
2020-07-20 21:33:22 +0500

    implementation of the second method v.2

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,120 +1,176 @@
 package task1;
 
-
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-import org.w3c.dom.ls.LSOutput;
-
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
-	static String[] firstNames = {"Ivan", "Petr", "Andrey", "Evgeny"};
-	static String[] lastNames = {"Ivanov", "Petrov", "Andreev", "Sidorov"};
-	static int numberOfUniqNames = firstNames.length * lastNames.length;
 
-	public static void main(String[] args) {
-		// TODO: 7/15/20 hi from outside
-		first();
-		second();
+    public static void main(String[] args) {
+        // TODO: 7/15/20 hi from outside
+        first();
+        second();
 
-	}
+    }
 
 
-
-	public static void first(){
-		// TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
-		ArrayList<Person> people = new ArrayList<>();
-
-
-		Person иванПетров = new Person("Иван Петров", 13, 8, 1956);
-		people.add(иванПетров);
-		people.add(new Person("Иван Иванов", 12, 5, 1985));
-		people.add(new Person("Иван Иванов", 12, 5, 1915));
-
-		Person oldestPerson = people.get(0);
-		for (Person person : people) {
-			boolean answer =  oldestPerson.isOlderThan(person);
-			if (!answer) {
-				oldestPerson = person;
-			}
-		}
-		System.out.println(oldestPerson.birthDate.year);
+    public static void first() {
+        // TODO: 1/26/20 ПЕРВЫЙ ЭТАП создать 5 разных человек и вывести имя старшего
+        ArrayList<Person> people = new ArrayList<>();
 
 
-	}
+        Person иванПетров = new Person("Иван Петров", 13, 8, 1956);
+        people.add(иванПетров);
+        people.add(new Person("Иван Иванов", 12, 5, 1985));
+        people.add(new Person("Иван Иванов", 12, 5, 1915));
 
-	public static void second(){
-		// TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
-		ArrayList<Person> peoples = new ArrayList<>();
-		while (peoples.size() <= numberOfUniqNames) {
-			peoples.add(new Person(generateName(), generateBirthDate()));
-		}
-		for (Person people : peoples) {
-			System.out.println(people);
-		}
-	}
+        Person oldestPerson = people.get(0);
+        for (Person person : people) {
+            boolean answer = oldestPerson.isOlderThan(person);
+            if (!answer) {
+                oldestPerson = person;
+            }
+        }
+        //System.out.println(oldestPerson.birthDate.year);
 
 
-	private static BirthDate generateBirthDate() {
-		Random r = new Random();
-		return new BirthDate(r.nextInt(30) + 1, r.nextInt(11) + 1, r.nextInt(110) + 1910);
-	}
+    }
 
-	private static Name generateName() {
-		Random r = new Random();
-		return new Name(firstNames[r.nextInt(firstNames.length)],
-				lastNames[r.nextInt(lastNames.length)]);
-	}
+    public static void second() {
+        // TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
+
+        int count = 100;
+        while (count > 0) {
+            System.out.println(new Person(new Name(), new BirthDate()));
+            count--;
+        }
+    }
 
 }
 
-@ToString
-@AllArgsConstructor
+
 class Name {
 
-	String firstName;
-	String lastName;
+    String firstName;
+    String lastName;
 
+
+    private static final ArrayList<String> firstNames = new ArrayList<>();
+    private static final ArrayList<String> lastNames = new ArrayList<>();
+    private Random randomNumber;
+
+
+    static {
+        firstNames.add("Ivan");
+        firstNames.add("Andrey");
+        firstNames.add("Petr");
+        firstNames.add("Evgeny");
+
+        lastNames.add("Ivanov");
+        lastNames.add("Petrov");
+        lastNames.add("Andreev");
+        lastNames.add("Sidorov");
+    }
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Name() {
+        try {
+            randomNumber = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        getRandomName();
+    }
+
+    private Name getRandomName() {
+        this.firstName = firstNames.get(randomNumber.nextInt(firstNames.size()));
+        this.lastName = lastNames.get(randomNumber.nextInt(lastNames.size()));
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + ' ' + lastName + ' ';
+    }
 }
-@ToString
+
+
 class BirthDate {
 
-	int year;
-	int month;
-	int day;
-	LocalDate localDate;
+    int year;
+    int month;
+    int day;
+    LocalDate localDate;
+    private Random randomNumber;
 
-	public BirthDate(int day, int month, int year) {
-		this.day = day;
-		this.month = month;
-		this.year = year;
-		localDate = LocalDate.of(year, month ,day);
-	}
+    public BirthDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.localDate = LocalDate.of(year, month, day);
+    }
 
-	public boolean earlyThan(BirthDate birthDate) {
-		return this.localDate.isBefore(birthDate.localDate);
-	}
+    public boolean earlyThan(BirthDate birthDate) {
+        return this.localDate.isBefore(birthDate.localDate);
+    }
+
+    public BirthDate() {
+        try {
+            this.randomNumber = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        getRandomDate();
+    }
+
+    private BirthDate getRandomDate() {
+        this.day = randomNumber.nextInt(30) + 1;
+        this.month = randomNumber.nextInt(11) + 1;
+        this.year = randomNumber.nextInt(110) + 1910;
+        try {
+            this.localDate = LocalDate.of(year, month, day);
+        } catch (DateTimeException e) {
+            this.localDate = LocalDate.of(year, month, day - 3);
+        }
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return localDate + "";
+    }
 }
-@ToString
+
+
 class Person {
 
-	Name name;
-	BirthDate birthDate;
+    Name name;
+    BirthDate birthDate;
 
 
-	public Person(String name, int day, int month, int year) {
-		birthDate = new BirthDate(day, month, year);
-		// TODO: 15.07.2020 implement
-	}
+    public Person(String name, int day, int month, int year) {
+        birthDate = new BirthDate(day, month, year);
+        // TODO: 15.07.2020 implement
+    }
 
-	public Person(Name name, BirthDate birthDate) {
-		this.name = name;
-		this.birthDate = birthDate;
-	}
+    public Person(Name name, BirthDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 
-	public boolean isOlderThan(Person person) {
-		return this.birthDate.earlyThan(person.birthDate);
-	}
+    public boolean isOlderThan(Person person) {
+        return this.birthDate.earlyThan(person.birthDate);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + birthDate;
+    }
 }
