e04324de126113a1ea0fda516d354fb4e7c09040
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-07-23 02:26:29 +0300

    add comments

--- src/main/java/task1/BirthDate.java
+++ src/main/java/task1/BirthDate.java
@@ -2,19 +2,20 @@ package task1;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Random;
 
 public class BirthDate {
 
+    private static final LocalDate minDate = LocalDate.of(1910, 1, 1);
+    private static final LocalDate maxDate = LocalDate.of(2019, 12, 31);
+    protected static final int MIN_AGE = 18;
+    protected static final int MAX_AGE = 120;
     int year;
     int month;
     int day;
     LocalDate localDate;
     private Random randomNumber;
-    private static final LocalDate minDate = LocalDate.of(1910, 1,1);
-    private static final LocalDate maxDate = LocalDate.of(2019, 12, 31);
 
 
     public BirthDate(int day, int month, int year) {
@@ -22,10 +23,6 @@ public class BirthDate {
         this.month = month;
         this.year = year;
         this.localDate = LocalDate.of(year, month, day);
-    }
-
-    public boolean earlyThan(BirthDate birthDate) {
-        return this.localDate.isBefore(birthDate.localDate);
     }
 
     public BirthDate() {
@@ -37,7 +34,15 @@ public class BirthDate {
         getRandomDate();
     }
 
+    public boolean earlyThan(BirthDate birthDate) {
+        return this.localDate.isBefore(birthDate.localDate);
+    }
+
     private BirthDate getRandomDate() {
+        //     TODO: 7/23/20 или так, наглядней, и не придется программу переписывать или перезапускать чтобы "стрелки подвести"
+        LocalDate minDate = LocalDate.now().minusYears(MIN_AGE);
+        LocalDate maxDate = LocalDate.now().minusYears(MAX_AGE);
+
         int minDay = (int) minDate.toEpochDay();
         int maxDay = (int) maxDate.toEpochDay();
         long randomDate = minDay + randomNumber.nextInt(maxDay - minDay);
