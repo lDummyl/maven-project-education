d1cde0a92e606f7ce1dc6a261f7068c7cf0ee594
Alex <alexander.lusher@gmail.com>
2020-07-28 23:24:40 -0700

    Added new file with Class example01

--- src/main/java/task1/example01.java
+++ src/main/java/task1/example01.java
@@ -1,7 +1,31 @@
 package task1;
 
+// This small example computes standard deviation
+
+import java.util.Random;
+
 public class example01 {
+    public static final int TOTAL_ELEMENTS = 80;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Random random = new Random();
+        int[] actual_values = new int[TOTAL_ELEMENTS];
+        int[] measured_values = new int[TOTAL_ELEMENTS];
+        int square_sum = 0;
+        int i = 0;
+        while (i < TOTAL_ELEMENTS) {
+            int curr_actual_value = random.nextInt(100);
+            actual_values[i] = curr_actual_value;
+            int curr_measured_value = random.nextInt(100);
+            measured_values[i] = curr_measured_value;
+            System.out.println("Measured value: " + curr_measured_value +
+                    " Actual value: " + curr_actual_value +
+                    " Difference: " + (curr_measured_value - curr_actual_value));
+            square_sum += Math.pow(curr_measured_value - curr_actual_value, 2);
+            i += 1;
+        }
+        // TO-DO: I cannot print the arrays actual_values and measured_values
+        System.out.println("The standard deviation: " + Math.pow(square_sum / TOTAL_ELEMENTS, 1 / 2));
+
     }
 }
