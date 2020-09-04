4e394e66af4b1bc856c656bcac64189c68627b7e
Alex <alexander.lusher@gmail.com>
2020-09-03 23:47:50 -0700

    Added animals

--- src/main/java/customPackage/people/Doctor.java
+++ src/main/java/customPackage/people/Doctor.java
@@ -2,12 +2,22 @@ package customPackage.people;
 
 public class Doctor extends Person {
 
+
     public Doctor(String name) {
         super(name);
     }
 
-    public boolean cure() {
-        return true;
+    public String curePatient(String name, String health) {
+        if (name == "Dolly" || name == "Pirate") {
+            health = null;
+            System.out.println("Doctor treats humans only");
+        } else {
+            if (health == "ill") {
+                health = "healthy";
+            } else {
+                System.out.println("No need to see a doctor!");
+            }
+        }
+        return health;
     }
 }
-
--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -2,20 +2,33 @@ package task3;
 
 /*
 
- Создать косольное приложение: великий и могущественный оракул,
- который отвечает на вопрос исходя из ряда правил
- Если вопрос сликом длинный, говорит будь лаконичней
- слишком короткий будь красноречивее
- проверяет на наличие вопроса что, где, когда, и тд...
- если их больше одного, ты задаешь слишком много вопросов
- если ни одного, то не еслышу вопроса в твоих речах
+ Создать консольное приложение: великий и могущественный оракул,
+ который отвечает на вопрос исходя из ряда правил.
+ Если вопрос слишком длинный, говорит будь лаконичней,
+ слишком короткий - будь красноречивее
+ проверяет на наличие вопроса что, где, когда, и т.д...
+ если их больше одного - ты задаешь слишком много вопросов
+ если ни одного, то не слышу вопроса в твоих речах
  на каждый из этих вопросов, если он один дается заготовленный мудрый ответ
  оракул может рандомно нахамить или стукнуть палкой
  может устать и сделать перерыв на 10 сек до минуты,
  сообщая тебе сколько еще ждать в ответ на вопросы.
 
- создать отчет о вопросах ответах виде Josn фаила используя
+ создать отчет о вопросах ответах виде Josn файла, используя
  Jackson Object mapper и положить его в ресурсы
+
+Algorithm
+  - create Josn list array with answers
+ - Main routine
+ 		- read from Josn file into list array
+ 		- endless loop that check for input
+ Upon new entry, we count question marks and full stops (regular expression)
+ If counter is great than 1, then requester is verbose and asked to be more concise
+ If no question marks are found, then system's response is "What's your question?"
+ If counter is equal to 1, then system's response is randomly picked from Josn file:
+    list array
+    	sleep timer 10 sec
+    	button strike
 
 */
 
