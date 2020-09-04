6047d784769e5e962feee51f3cffb9182a1480bb
Alex <alexander.lusher@gmail.com>
2020-09-03 23:48:54 -0700

    Added animals

---
+++ src/main/java/customPackage/animals/Reflection1.java
@@ -0,0 +1,22 @@
+package customPackage.animals;
+
+public class Reflection1 {
+
+    private final int i = 10;
+
+    public void methoda() {
+
+        System.out.println("method1");
+    }
+
+    public void methodb() {
+
+        System.out.println("method2");
+    }
+
+    public void methodc() {
+
+        System.out.println("method3");
+    }
+
+}
