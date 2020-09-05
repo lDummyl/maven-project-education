8497d5914e018ff1898b32ce8769f876c72609f4
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-09-05 15:07:48 +0300

    add comment

--- src/main/java/task3/OracleExceptions.java
+++ src/main/java/task3/OracleExceptions.java
@@ -1,5 +1,6 @@
 package task3;
 
+// TODO: 9/5/20 наследуй свои эксепшены от RuntimeException догадайся почему
 public class OracleExceptions extends Exception{
 
     public OracleExceptions(String msg, int maxPossibility){
