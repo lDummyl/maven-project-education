bf13ff4b4145e14f5466c76d2c4c790b34cad797
Oblivion888 <Sakromat234@gmail.com>
2020-09-05 14:10:04 +0300

    Реализация через LocalTime

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -3,6 +3,7 @@ package task1;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.time.*;
 
 public class Main {
 
@@ -18,8 +19,9 @@ public class Main {
 
         int oldest = 0;
 
+
         Person person = new Person("Иван Иванов", 20, 11, 1945);
-        Person person2 = new Person("Григорий Зуев", 14, 9, 1911);
+        Person person2 = new Person("Григорий Зуев", 14, 4, 1911);
         Person person3 = new Person("Степан Трешков", 4, 8, 1993);
         Person person4 = new Person("Виктор Триванов", 10, 6, 1964);
         Person person5 = new Person("Ник Теренов", 10, 6, 1911);
@@ -31,28 +33,17 @@ public class Main {
         list.add(person4);
         list.add(person5);
 
-        int min = list.get(0).birthDate.day + list.get(0).birthDate.month * 30 + list.get(0).birthDate.day * 365;
+        LocalDate min = LocalDate.of(list.get(0).birthDate.year, list.get(0).birthDate.month, list.get(0).birthDate.day);
 
-        ArrayList<Integer> days = new ArrayList<Integer>();
         for (int i = 0; i < list.size(); i++) {
-            days.add(list.get(i).birthDate.day + list.get(i).birthDate.month * 30 + list.get(i).birthDate.day * 365);
-            // TODO: 9/4/20 правильно, допущения в рамках обстоятельств это нормально, почитай про LocalDateTime вообще пакет java.time
-            //  посмотрим как сможешь его использовать
-            //Работает без учёта дней в месмяце и высокосности годов
-        }
-        for (int i = 0; i < list.size(); i++) {
-            if (days.get(i) < min) {
-                min = days.get(i);
+            if (LocalDate.of(list.get(i).birthDate.year, list.get(i).birthDate.month, list.get(i).birthDate.day).isBefore(min)) {
+                min = LocalDate.of(list.get(i).birthDate.year, list.get(i).birthDate.month, list.get(i).birthDate.day);
                 oldest = i;
             }
         }
-
-        //System.out.println(days);
-
-        // TODO: 9/3/20 Hi there!
-
         System.out.println(list.get(oldest).name.firstName);
     }
+
 
     public static void second() {
         // TODO: 1/26/20 ВТОРОЙ ЭТАП создать коллекцию имен и создавать людей в цикле, подставляя имена и возраст рандомно.
@@ -107,7 +98,7 @@ class BirthDate {
 class Person {
     public Person(Name name, int day, int month, int year) {
         this.name = name;
-        this.birthDate = new BirthDate(day, month, year);
+        this.birthDate = new BirthDate(year, month, day);
     }
 
     Name name;
@@ -116,7 +107,7 @@ class Person {
 
     public Person(String name, int day, int month, int year) {
         String[] split = name.split(" ");
-        this.birthDate = new BirthDate(day, month, year);
+        this.birthDate = new BirthDate(year, month, day); //Не в правильном порядке передавались параметры в birthdate
         this.name = new Name(split[0], split[1]);
     }
 
