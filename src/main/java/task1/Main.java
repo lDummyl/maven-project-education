93eff38b88cd026e0e171a231447dea5f1d1634d
Oblivion888 <Sakromat234@gmail.com>
2020-09-07 19:53:50 +0300

    Перехвачены все исключения, которые вызывали неправильное создание объекта Вернул класс birthdate PersonProcessor вынесен в отдельный клапсс

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -1,10 +1,7 @@
 package task1;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
+import java.util.*;
 import java.time.*;
 
 public class Main {
@@ -20,17 +17,15 @@ public class Main {
     }
 
     public static void second() {
-        Generator generator = new Generator();
-        ArrayList<Person> list = (ArrayList<Person>) generator.generatePersons(100).clone();
-        //  for (int i = 0; i < list.size(); i++) {
-        //     System.out.println(list.get(i).name.firstName+" "+list.get(i).name.lastName+" "+list.get(i).birthDate);
-        // }
-        System.out.println(list.size());
+        PersonsProcessor generator = new PersonsProcessor();
+        generator.generatePersons(100);
+        ArrayList<Person> list = generator.generatePersons(100);
+        generator.oldestSearch(list);
+
     }
 }
 
 class Name {
-
     String firstName;
     String lastName;
 
@@ -41,37 +36,31 @@ class Name {
     }
 }
 
+class BirthDate {
+
+    int year;
+    int month;
+    int day;
+
+    public BirthDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
+
 class Person {
     Name name;
-    LocalDate birthDate;
+    LocalDate localbirthDate;
 
-    public Person(String name, int day, int month, int year) {
-        String[] split = name.split(" ");
-        try {
-            this.birthDate = LocalDate.of(year, month, day);
-        } catch (DateTimeException e) {
-            System.out.println("Недопустимая дата");
-        }
+    //public Person(String name, int day, int month, int year) {
+    //    this.birthDate = LocalDate.of(year, month, day);
+    //     this.name = new Name(name);
+
+
+    public Person(String name, LocalDate birthDate) {
         this.name = new Name(name);
+        this.localbirthDate = birthDate;
     }
 }
 
-class Generator {
-    public ArrayList<Person> generatePersons(int numberOfPerons) {
-        Random random = new Random();
-        String[] fnames = {"Иван", "Григорий", "Василий", "Эдди", "Сергей", "Александр", "Роланд", "Стивин", "Виктор", "Томас", "Айзек"};
-        String[] lnames = {"Паршин", "Иванов", "Анхимов", "Быков", "Вихров", "Артёмов", "Фёдоров", "Аркадьев", "Гришин", "Хрусталёв", "Азимов"};
-
-        HashSet<String> names = new HashSet<String>();
-        while (names.size() < numberOfPerons) {
-            names.add(fnames[random.nextInt(fnames.length)] + " " + lnames[random.nextInt(lnames.length)]);
-        }
-        ArrayList<Person> persons = new ArrayList<>();
-        Iterator<String> itr = names.iterator();
-        while (itr.hasNext()) {
-            String name = itr.next();
-            persons.add(new Person(name, 1 + random.nextInt(30), 1 + random.nextInt(11), 1 + random.nextInt(2020)));
-        }
-        return persons;
-    }
-}
---
+++ src/main/java/task1/PersonsProcessor.java
@@ -0,0 +1,56 @@
+package task1;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
+public class PersonsProcessor {
+    public ArrayList<Person> generatePersons(int numberOfPerons) {
+        Random random = new Random();
+        String[] fnames = {"Иван", "Григорий", "Василий", "Эдди", "Сергей", "Александр", "Роланд", "Стивин", "Виктор", "Томас", "Айзек"};
+        String[] lnames = {"Паршин", "Иванов", "Анхимов", "Быков", "Вихров", "Артёмов", "Фёдоров", "Аркадьев", "Гришин", "Хрусталёв", "Азимов"};
+
+        HashSet<String> names = new HashSet<String>();
+        ArrayList<String> namesArray = new ArrayList<String>();
+
+        while (names.size() < numberOfPerons) {
+            names.add(fnames[random.nextInt(fnames.length)] + " " + lnames[random.nextInt(lnames.length)]);
+        }
+        Iterator<String> itr = names.iterator();
+        while (itr.hasNext()) {
+            String name = itr.next();
+            namesArray.add(name);
+        }
+
+        ArrayList<Person> persons = new ArrayList<Person>();
+        ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
+
+        while (dates.size() < numberOfPerons) {
+            try {
+                dates.add(LocalDate.of(random.nextInt(1 + 2019), random.nextInt(1 + 11), random.nextInt(1 + 30)));
+            } catch (DateTimeException e) {
+            }
+        }
+
+        for (int i = 0; i < numberOfPerons; i++) {
+            persons.add(new Person(namesArray.get(i), dates.get(i)));
+        }
+        return persons;
+    }
+
+    public void oldestSearch(ArrayList<Person> list) {
+        int oldest = 0;
+        LocalDate min = list.get(0).localbirthDate;
+        ArrayList<Person> sorted = list;
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).localbirthDate.isBefore(min)) {
+                min = list.get(i).localbirthDate;
+                oldest = i;
+            }
+        }
+        System.out.println(list.get(oldest).name.firstName);
+    }
+}
