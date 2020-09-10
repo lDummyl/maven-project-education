d1d09ac181d580b897a68eee8c3c448c26aa46b8
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-09-10 10:38:23 +0300

    add comment

--- src/main/java/task1/PersonsProcessor.java
+++ src/main/java/task1/PersonsProcessor.java
@@ -17,6 +17,7 @@ public class PersonsProcessor {
 
     public void oldestSearch(ArrayList<Person> list) {
         int oldest = 0;
+        // TODO: 9/10/20 подумай насчет инкапсуляции и здесь, чтобы зделать код лаконичней и наглядней
         LocalDate min = list.get(0).birthDate.localBirthDate;
         for (int i = 1; i < list.size() - 1; i++) {
             if (list.get(i).birthDate.localBirthDate.isBefore(min)) {
