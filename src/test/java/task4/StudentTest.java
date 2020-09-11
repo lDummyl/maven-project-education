54eb9ffde4c6ec4d9c954418707c076fdda1e5e1
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-09-11 14:59:17 +0300

    add comment

--- src/test/java/task4/StudentTest.java
+++ src/test/java/task4/StudentTest.java
@@ -13,6 +13,9 @@ public class StudentTest {
         String s = "Как ";
         String ss = "купить ";
         String sss = "машину?";
+        // TODO: 9/11/20 забей ты пока на моки, все это моканье на данном этапе это так для общего понимания.
+        //  Тест же задает контрект взаимодействия тестируемых узлов, а потом уже идет его реализация.
+
         Student studentMock = Mockito.mock(Student.class);
         Mockito.when(studentMock.generatorQuestions().thenReturn(studentMock.keyQuestions.get(0)+" "+
                 studentMock.questionContextOne.get(0)+" "+studentMock.questionContextTwo.get(0)));
