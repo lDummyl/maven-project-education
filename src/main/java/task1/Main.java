83c246f6b79bc5a99676c8d1a77540a05f310b20
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-09-12 18:48:08 +0300

    add comment

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -3,6 +3,8 @@ package task1;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -19,6 +21,7 @@ public class Main {
     public static void second() {
         PersonsProcessor generator = new PersonsProcessor();
         // TODO: 9/10/20 сделай так чтобы все созданные 100к человек были уникальны
+        // TODO: 9/12/20 увы незачет, следующая попытка...
         ArrayList<Person> list = (ArrayList<Person>) generator.generatePersons(100000).clone();
         System.out.println(list.size());
         System.out.println(Person.oldest.name.firstName);
