89da82960f6571d58f9d1925635e0d0acf9c74e1
Oblivion888 <Sakromat234@gmail.com>
2020-10-07 21:17:44 +0300

    Добавил тестов

--- src/test/java/CollectionWork/GeneratorTest.java
+++ src/test/java/CollectionWork/GeneratorTest.java
@@ -5,6 +5,7 @@ import task1.Person;
 import task1.PersonsProcessor;
 
 import java.awt.font.NumericShaper;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.TreeSet;
@@ -32,8 +33,33 @@ public class GeneratorTest {
         for (Wanderer w : wanderers) {
             assertTrue(w.health > expecdeadHealth);
         }
-
     }
 
 
+    @Test
+    public void arrayPersonGenerator() {
+        Generator generator = new Generator();
+        int expected = 40_000;
+        ArrayList<Wanderer> wanderers = generator.arrayPersonGenerator(expected);
+        assertEquals(expected, wanderers.size());
+    }
+
+    @Test
+    public void uniArrayPersonGenerator() {
+        Generator generator = new Generator();
+        int expected = 40_000;
+        ArrayList<Wanderer> wanderers = generator.uniArrayPersonGenerator(expected);
+        assertEquals(expected, wanderers.size());
+
+        boolean current = true;
+        for (int i = 0; i < wanderers.size(); i++) {
+            for (int j = i+1; j <wanderers.size() ; j++) {
+                if (wanderers.get(j).equals(wanderers.get(i))){
+                    current = false;
+                }
+            }
+            assertTrue(current);
+        }
+
+    }
 }
