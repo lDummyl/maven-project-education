37061ab37e7496f021314ae1aaf2bfab7c613450
alexlusher <alexander.lusher@gmail.com>
2020-10-14 07:28:44 -0700

    Removed comments

---
+++ src/main/java/customPackage/interaction/MyExceptionPractice03.java
@@ -0,0 +1,8 @@
+package customPackage.interaction;
+
+public class MyExceptionPractice03 {
+    public static void main(String[] args) {
+        NullPointerException e = new NullPointerException();
+        System.out.println("What about this?");
+    }
+}
