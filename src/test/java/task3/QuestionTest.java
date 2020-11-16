d708a6d19530c8b769896caf4a83d31fe71e378a
dmitrii.ludov <dmitrii.ludov@betoola.com>
2020-11-16 14:29:18 +0300

    add comment

---
+++ src/test/java/task3/QuestionTest.java
@@ -0,0 +1,43 @@
+package task3;
+
+import junit.framework.TestCase;
+
+public class QuestionTest extends TestCase {
+
+    public void testGetKeyWort() {
+    }
+
+    public void testSpecialQuestCheck() {
+        String noKey = "asdgg g erg ";
+
+
+        for (String s : Question.specialQuests) {
+            Question question = new Question(s + "asdgg g erg ");
+            int expectedSize = 1;
+            assertEquals(expectedSize, question.keyWort.size());
+            assertEquals(s, question.getKeyWort().get(0));
+        }
+
+        for (int i = 0; i < Question.specialQuests.length - 1; i++) {
+            int expectedSize = 2;
+            Question question = new Question(Question.specialQuests[i] + "asdgg g erg " + Question.specialQuests[i + 1]);
+            assertEquals(expectedSize, question.keyWort.size());
+        }
+    }
+
+
+    public void testGetQuestionText() {
+        Question some = new Question("Что то там");
+        assertEquals("Что то там", some.questionText);
+    }
+
+    public void testGetQuestionLength() {
+        Question some = new Question("Что то там");
+        assertEquals(10, some.questionLength);
+    }
+
+    public void testTestGetKeyWort() {
+        Question some = new Question("Что то там");
+        assertEquals("Что", some.getKeyWort().get(0));
+    }
+}
---
+++ src/test/java/task3/RegularAnswerTest.java
@@ -0,0 +1,16 @@
+package task3;
+
+import junit.framework.TestCase;
+/*
+
+public class RegularAnswerTest extends TestCase {
+
+
+    public void testGetAnswers() {
+        for (String s :
+                Question.specialQuests) {
+            System.out.println(RegularAnswer.getAnswers().get(s));
+        }
+    }
+*/
+//}
