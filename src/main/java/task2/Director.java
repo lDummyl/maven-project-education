78622fc99bfa5f54309c1bf24489c0f092203fb7
Dima Ludov <lDummyl>
2020-11-22 20:06:39 +0300

    add comment

--- src/main/java/task2/Director.java
+++ src/main/java/task2/Director.java
@@ -1,26 +1,39 @@
 package task2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class Director {
 
-    private Random randomChoose;
+	private Random randomChoose;
 
-    public Employee chooseEmployee(Employee employee, Map<Class<?>, List<Employee>> employees, Class<?> employeeClass) {
-        if (employee == null) { //если работник уже присвоен то выходим
-            List<Employee> tempList = employees.get(employeeClass);
-            if (tempList != null && tempList.size() >= 10) {
-                randomChoose = new Random();
-                Employee tempEmployee = tempList.get(randomChoose.nextInt(employees.size()));
-                tempList.remove(tempEmployee); // удаляем работника который уже выбран
-                employees.put(employeeClass, tempList);
-                return tempEmployee;
-            } else {
-                return null;
-            }
-        }
-        return employee;
-    }
+
+	// TODO: 22.11.2020 все то же самое изучай дженерики
+	public <T extends Employee> T chooseEmployee(Map<Class<?>, List<Employee>> employees, Class<T> employeeClass) {
+		List<Employee> tempList = employees.get(employeeClass);
+		if (tempList != null && tempList.size() >= 10) {
+			randomChoose = new Random();
+			Employee tempEmployee = tempList.get(randomChoose.nextInt(employees.size()));
+			tempList.remove(tempEmployee); // удаляем работника который уже выбран
+			employees.put(employeeClass, tempList); // TODO: 22.11.2020  лист и так изменится повтроно его класть не нужно для этого, потому что ты получаешь не лист, а ссылку на него
+			if (employeeClass.isInstance(employeeClass)) {
+				return employeeClass.cast(tempEmployee);
+			} else{
+				throw new IllegalStateException("WTF?");
+			}
+		}
+		return null;// вметсо null лучше возвращать Optional.empty() почитай, подумай как
+	}
+
+	public static void main(String[] args) {
+		Director director = new Director();
+
+		HashMap<Class<?>, List<Employee>> map = new HashMap<>();
+		Secretary secretary = director.chooseEmployee(map, Secretary.class);
+		Lawyer lawyer = director.chooseEmployee(map, Lawyer.class);
+
+	}
+
 }
--- src/main/java/task2/Office.java
+++ src/main/java/task2/Office.java
@@ -20,12 +20,13 @@ public class Office {
 	//претенденты идут по одному, когда их достаточно,
 	// то диретор принимает решение и берет одного в штат
 
+
 	void invitePeople(Object human){
 		hr.checkEmployee(human);
-		secretary = (Secretary) director.chooseEmployee(secretary, hr.getListEmployee(), Secretary.class);
-		lawyer = (Lawyer) director.chooseEmployee(lawyer, hr.getListEmployee(), Lawyer.class);
-		security = (Security) director.chooseEmployee(security, hr.getListEmployee(), Security.class);
-		firstAccountant = (Accountant) director.chooseEmployee(firstAccountant, hr.getListEmployee(), Accountant.class);
-		secondAccountant = (Accountant) director.chooseEmployee(secondAccountant, hr.getListEmployee(), Accountant.class);
+//		secretary = director.chooseEmployee(hr.getListEmployee(), Secretary.class).;
+//		lawyer = (Lawyer) director.chooseEmployee(lawyer, hr.getListEmployee(), Lawyer.class);
+//		security = (Security) director.chooseEmployee(security, hr.getListEmployee(), Security.class);
+//		firstAccountant = (Accountant) director.chooseEmployee(firstAccountant, hr.getListEmployee(), Accountant.class);
+//		secondAccountant = (Accountant) director.chooseEmployee(secondAccountant, hr.getListEmployee(), Accountant.class);
 	}
 }
