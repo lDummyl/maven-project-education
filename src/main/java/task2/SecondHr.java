e87994ca506fbfa2a9102f277622b032e767c0be
oblivion <Salromat234@gmail.com>
2020-11-24 22:51:22 +0300

    Дополнение

--- src/main/java/task2/SecondHr.java
+++ src/main/java/task2/SecondHr.java
@@ -21,23 +21,13 @@ public class SecondHr implements Hr {
     // TODO: 24.11.2020 Доделать перенос интервью Hr
     // TODO: 11/16/20  да, это норма! А вот Unchecked cast это то что делать не стоит так жк как и проверять через instanceof,
     //  но с этим у тебя все получится.
-    public void addCandidate(Object candidate) {
-        Class<?> candidateClass = candidate.getClass();
-        if (!(candidate instanceof Worker)) {
-            throw new IllegalStateException("Wrong object");
-        }
-        List<Worker> workers = candidates.get(candidateClass);
-        if (workers != null) {
-            workers.add((Worker) candidate);
-        } else {
+    // С дженериком вроде выглядит красивее
+    public<T extends Worker> void addCandidate(T candidate) {
+        Class<? extends Worker> aClass = candidate.getClass();
 
-            if (workers == null) {
-                throw new NullPointerException("Пустой список кандидатов");
-            } else {
-                List<Worker> candidates = workers.stream().filter(Worker::passInterview).collect(Collectors.toList());
-            }
-            candidates.put(candidateClass, (List<Worker>) candidates);
-        }
+        List<T> workers = new ArrayList<>();
+
+        workers.add(candidate);
     }
 
     @Override
