938008116fabf483ef95244891677353b4ccf829
oblivion <Salromat234@gmail.com>
2020-12-06 18:25:53 +0300

    sdf

--- src/test/java/task4/oracleQuestionProviderTest.java
+++ src/test/java/task4/oracleQuestionProviderTest.java
@@ -2,14 +2,11 @@ package task4;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import task3.Resolution;
+
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
 
-import static org.junit.Assert.*;
 
 public class oracleQuestionProviderTest {
     ObjectMapper mapper = new ObjectMapper();
