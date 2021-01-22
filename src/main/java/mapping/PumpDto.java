3f1fe2119a7609fb1c332b2dd7736e7bbfea508f
Oblivion888 <Sakromat234@gmail.com>
2021-01-22 21:09:26 +0300

    mapper

--- src/main/java/mapping/PumpDto.java
+++ src/main/java/mapping/PumpDto.java
@@ -10,5 +10,4 @@ import lombok.Setter;
 public class PumpDto {
     String a;
     String b;
-
 }
--- src/main/java/mapping/PumpMapper.java
+++ src/main/java/mapping/PumpMapper.java
@@ -4,12 +4,11 @@ import java.util.*;
 
 public class PumpMapper {
     public PumpDto map(Pump pump) {
-        //  PumpDto pumpDto = new PumpDto();
-        //  Integer fieldA = pump.getA().orElseThrow(() -> new RuntimeException("field a absent"));
-        // pumpDto.setA(fieldA);
-        //    throw new Not
-        //}
-        throw new UnsupportedOperationException();
+        PumpDto pumpDto = new PumpDto();
+        Integer fieldA = pump.getA().orElseThrow(() -> new RuntimeException("field a absent"));
 
+        pumpDto.setA(fieldA.toString());
+        pumpDto.setB(pumpDto.getB());
+        return pumpDto;
     }
 }
