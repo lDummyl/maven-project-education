87cb2d7d7dbb0248b9acaa66a29378fd48b8fc2a
Oblivion888 <Sakromat234@gmail.com>
2021-02-08 00:18:25 +0300

    current stable

--- src/test/java/task6/LagrangePolynomialTest.java
+++ src/test/java/task6/LagrangePolynomialTest.java
@@ -3,6 +3,8 @@ package task6;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class LagrangePolynomialTest extends TestCase {
     @Test
     public void testInterpolateLagrangePolynomial() {
@@ -27,5 +29,57 @@ public class LagrangePolynomialTest extends TestCase {
         Double lagrange = lagrangePolynomial.getValueY(x);
 
         assertEquals(testAnswer, lagrange);
+    }
+    @Test
+    public void testWrongData() {
+        Double x= 13.5;
+
+        Double testAnswer = x*x*x + 3*x*x + 3*x + 1;
+
+
+        int size = 10;
+
+
+        double[] xValues = new double[size];
+        double[] yValues = new double[size];
+
+        xValues[0] = 0.0;
+        yValues[0] = 0.0;
+        yValues[1] = 0.0;
+
+        try {
+            LagrangePolynomial lagrangePolynomial = new LagrangePolynomial(xValues, yValues);
+        } catch (Exception e) {
+            assertNotNull(e);
+        }
+    }
+
+
+    @Test
+    public void testListLagrangePolynomialConstr() {
+        int size = 10;
+        ArrayList<Double> xValues = new ArrayList<>();
+        ArrayList<Double> yValues = new ArrayList<>();
+        for (int i = 0; i < size; i++)
+        {
+            xValues.add((double) i);
+            yValues.add((double) (i*i*i + 3*i*i + 3*i + 1));
+        }
+
+        LagrangePolynomial lagrangePolynomial = new LagrangePolynomial(xValues, yValues);
+    }
+
+    @Test
+    public void testDoubleLagrangePolynomialConstr() {
+        int size = 10;
+        Double[] xValues = new Double[size];
+        Double[] yValues = new Double[size];
+        for (int i = 0; i < size; i++)
+        {
+            xValues[i] = (double) i;
+            yValues[i] = (double) i*i*i + 3*i*i + 3*i + 1;
+        }
+
+        LagrangePolynomial lagrangePolynomial = new LagrangePolynomial(xValues, yValues);
     }
 }
