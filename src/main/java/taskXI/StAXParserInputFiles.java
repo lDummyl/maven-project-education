3652da637edcc7661b74404844a5ede55a0c1774
LilyLillet <kruglikova_liliya@mail.ru>
2021-07-22 18:07:58 +0300

    домучила создание листов с объектами userlogs

--- src/main/java/taskXI/StAXParserInputFiles.java
+++ src/main/java/taskXI/StAXParserInputFiles.java
@@ -1,8 +1,8 @@
 package taskXI;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.*;
 import java.io.File;
@@ -17,72 +17,52 @@ public interface StAXParserInputFiles {
     default List<UserLogs> createUserLogs(File file) {
         //извлечение данных из файлов xml с использованием xsd (посредством xerces)
         List<UserLogs> list = new ArrayList<>();
-        boolean log = false;
-        boolean timestamp = false;
-        boolean userID = false;
-        boolean url = false;
-        boolean seconds = false;
-
         XMLInputFactory factory = XMLInputFactory.newInstance();
         try {
             XMLEventReader eventReader =
                     factory.createXMLEventReader(new FileReader(file));
+            UserLogs userLogs = new UserLogs();
             while (eventReader.hasNext()) {
-                UserLogs userLogs = new UserLogs();
-
-
-                XMLEvent event = eventReader.nextEvent();
-                switch (event.getEventType()) {
-                    case XMLStreamConstants.START_ELEMENT:
-                        StartElement startElement = event.asStartElement();
-                        String qName = startElement.getName().getLocalPart();
-                        if (qName.equalsIgnoreCase("log")) {
-                            log = true;
-                        } else if (qName.equalsIgnoreCase("timestamp")) {
-                            timestamp = true;
-                        } else if (qName.equalsIgnoreCase("userID")) {
-                            userID = true;
-                        } else if (qName.equalsIgnoreCase("url")) {
-                            url = true;
-                        } else if (qName.equalsIgnoreCase("seconds")) {
-                            seconds = true;
-                        }
-                        break;
-                    case XMLStreamConstants.CHARACTERS:
-                        Characters characters = event.asCharacters();
-                        if (log) {
-                            if (timestamp) {
-                                userLogs.setUnixTime(Long.parseLong(characters.getData()));
-                                timestamp = false;
-                            }
-                            if (userID) {
-                                userLogs.setUserID(characters.getData());
-                                userID = false;
-                            }
-                            if (url) {
-                                userLogs.setUrl(characters.getData());
-                                url = false;
-                            }
-                            if (seconds) {
-                                userLogs.setQtySecondsUserSpend(Integer.parseInt(characters.getData()));
-                                seconds = false;
-                            }
-                        }
+                XMLEvent nextEvent = eventReader.nextEvent();
+                if (nextEvent.isStartElement()) {
+                    StartElement startElement = nextEvent.asStartElement();
+                    switch (startElement.getName().getLocalPart()) {
+                        case "log":
+                            userLogs = new UserLogs();
+                            Attribute log = startElement.getAttributeByName(new QName("log"));
+                            if (log != null) {}
+                            break;
+                        case "timestamp":
+                            nextEvent = eventReader.nextEvent();
+                            userLogs.setUnixTime(Long.parseLong(nextEvent.asCharacters().getData()));
+                            break;
+                        case "userId":
+                            nextEvent = eventReader.nextEvent();
+                            userLogs.setUserID(nextEvent.asCharacters().getData());
+                            break;
+                        case "url":
+                            nextEvent = eventReader.nextEvent();
+                            userLogs.setUrl(nextEvent.asCharacters().getData());
+                            break;
+                        case "seconds":
+                            nextEvent = eventReader.nextEvent();
+                            userLogs.setQtySecondsUserSpend(Integer.parseInt(nextEvent.asCharacters().getData()));
+                            break;
+                    }
+                }
+                if (nextEvent.isEndElement()) {
+                    EndElement endElement = nextEvent.asEndElement();
+                    if (endElement.getName().getLocalPart().equals("log")) {
                         list.add(userLogs);
-                        break;
-                    case XMLStreamConstants.END_ELEMENT:
-                        EndElement endElement = event.asEndElement();
-                        if (endElement.getName().getLocalPart().equalsIgnoreCase("log")) {
-
-                        }
-                        break;
+                    }
                 }
             }
 
-        } catch (XMLStreamException | FileNotFoundException e) {
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         return list;
     }
-
 }
